990f82dd350aedfbcc3e0a426b72aa435411a0c6
Brian Ott <bott@b1-systems.de>
2024-09-30 15:44:42 +0200

    update: funcs to create, add, get, reapLoop and reap Cache

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -15,3 +15,40 @@ type cacheEntry struct {
     createdAt   time.Time
     val         []byte
 }
+
+func NewCache(interval time.Duration) Cache {
+    c := Cache{
+        cache:  make(map[string]cacheEntry),
+        mux:    &sync.Mutex{},
+    }
+
+    go c.reapLoop(interval)
+
+    return c
+}
+
+func (c *Cache) Add(key string, value []byte) {
+    c.mux.Lock()
+    defer c.mux.Unlock()
+    c.cache[key] = cacheEntry{
+        createdAt:  time.Now().UTC(),
+        val:        value,
+    }
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+    ticker := time.NewTicker(interval)
+    for range ticker.C {
+        c.reap(time.Now().UTC(), interval)
+    }
+}
+
+func (c *Cache) reap(now time.Time, last time.Duration) {
+    c.mux.Lock()
+    defer c.mux.Unlock()
+    for k, v := range c.cache {
+        if v.createdAt.Before(now.Add(-last)) {
+            delete(c.cache, k)
+        }
+    }
+}
